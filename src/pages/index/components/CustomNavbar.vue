2205684127e56e2791cc454877c09be302549ce6
chenzhibin <chenzb961@163.com>
2023-08-19 17:16:39 +0800

    首页ui

---
+++ src/pages/index/components/CustomNavbar.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// 获取屏幕边界到安全区域距离
+const { safeAreaInsets } = uni.getSystemInfoSync()
+const value = ref('')
+</script>
+
+<template>
+  <view class="navbar" :style="{ paddingTop: safeAreaInsets!.top + 8 + 'px' }">
+    <!-- logo文字 -->
+    <view class="logo">
+      <image
+        class="logo-image"
+        src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+      ></image>
+      <text class="logo-text">小张翼德的店铺</text>
+    </view>
+    <!-- 搜索条 -->
+    <view class="search">
+      <uni-easyinput
+        class="input-search"
+        v-model="value"
+        prefixIcon="search"
+        :inputBorder="false"
+        confirm-type="search"
+        placeholder="好安森糖尿病治疗仪"
+      ></uni-easyinput>
+      <text class="text-search">搜索</text>
+    </view>
+    <image class="info-image" src="@/static/images/navigator_info.png"></image>
+  </view>
+</template>
+
+<style lang="scss">
+/* 自定义导航条 */
+.navbar {
+  background: no-repeat center;
+  background-image: url(@/static/images/navigator_bg.png);
+  background-size: 100% 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  .logo {
+    display: flex;
+    align-items: center;
+    height: 56rpx;
+    padding-left: 24rpx;
+    .logo-image {
+      width: 56rpx;
+      height: 56rpx;
+      border-radius: 50%;
+    }
+    .logo-text {
+      width: 224rpx;
+      font-size: 32rpx;
+      font-weight: 500;
+      color: #333333;
+      padding-left: 16rpx;
+    }
+  }
+  .search {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 10rpx 0 24rpx;
+    margin: 44rpx 24rpx 0;
+    height: 60rpx;
+    color: #999999;
+    font-size: 28rpx;
+    background: rgba(0, 0, 0, 0.03);
+    border-radius: 200rpx;
+    .text-search {
+      width: 108rpx;
+      height: 52rpx;
+      background: linear-gradient(90deg, #ff9f1a 0%, #ff5e00 100%);
+      border-radius: 200rpx;
+      color: #ffffff;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+  .info-image {
+    height: 40rpx;
+    width: auto;
+    margin: 24rpx 48rpx;
+  }
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,111 @@
 <script setup lang="ts">
-//
+import { ref } from 'vue'
+import CustomNavbar from './components/CustomNavbar.vue'
+
+// 当前下拉刷新状态
+const isTriggered = ref(false)
+// 自定义下拉刷新被触发
+const onRefresherrefresh = async () => {
+  isTriggered.value = true
+  setTimeout(() => {
+    isTriggered.value = false
+  }, 2000)
+}
+// 滚动触底事件
+const onScrolltolower = () => {}
 </script>
 
 <template>
-  <view class="index">index</view>
+  <view class="index">
+    <!-- 自定义导航栏 -->
+    <CustomNavbar />
+    <!-- 滚动容器 -->
+    <scroll-view
+      enable-back-to-top
+      refresher-enabled
+      @scrolltolower="onScrolltolower"
+      @refresherrefresh="onRefresherrefresh"
+      :refresher-triggered="isTriggered"
+      refresher-background="#f6f8f9"
+      class="scroll-view"
+      scroll-y
+    >
+      <view class="brand-list">
+        <view class="brand" v-for="(item, i) in 12" :key="i">
+          <view class="logo-info">
+            <image
+              class="logo"
+              src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+            ></image>
+            <view class="info">
+              <view class="logo-text">SKII</view>
+              <view class="des">全部88个商品</view>
+            </view>
+          </view>
+          <image class="next" src="@/static/images/next.png"></image>
+        </view>
+      </view>
+    </scroll-view>
+  </view>
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f6f8f9;
+  height: 100%;
+  overflow: hidden;
+}
+.index {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  .scroll-view {
+    flex: 1;
+    overflow: hidden;
+  }
+  .brand-list {
+    padding: 0 24rpx;
+    padding-top: 24rpx;
+    .brand {
+      display: flex;
+      align-items: flex-end;
+      justify-content: space-between;
+      padding: 24rpx;
+      width: 702rpx;
+      height: 300rpx;
+      background: #000000;
+      margin-bottom: 24rpx;
+      border-radius: 20rpx;
+      .logo-info {
+        display: flex;
+        align-items: center;
+        .logo {
+          width: 100rpx;
+          height: 100rpx;
+          box-shadow: 0rpx 2rpx 8rpx 0rpx rgba(0, 0, 0, 0.2);
+          border-radius: 16rpx;
+        }
+        .info {
+          margin-left: 16rpx;
+          font-size: 44rpx;
+          font-weight: 500;
+          color: #ffffff;
+          line-height: 56rpx;
+          .des {
+            margin-top: 4rpx;
+            height: 36rpx;
+            font-size: 28rpx;
+            font-weight: 400;
+            color: #ffffff;
+            line-height: 36rpx;
+          }
+        }
+      }
+      .next {
+        width: 60rpx;
+        height: 60rpx;
+      }
+    }
+  }
+}
 </style>
